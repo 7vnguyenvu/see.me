710a91dc9a9ca558a790cd03c53e8a886e79ca46
Vune <nguyenvu2319@gmail.com>
2024-07-14 13:13:59 +0700

    fix: auto change icon again

--- src/hooks/useDynamicFavicon.ts
+++ src/hooks/useDynamicFavicon.ts
@@ -1,13 +1,17 @@
 import { useColorScheme } from "@mui/joy/styles";
-import { useLayoutEffect } from "react";
+import { useEffect } from "react";
 
 export function useDynamicFavicon() {
     const { mode } = useColorScheme();
 
-    useLayoutEffect(() => {
-        const favicon = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
-        if (favicon) {
-            favicon.href = mode === "dark" ? "/see.me-light.ico" : "/see.me-dark.ico";
-        }
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            const favicon = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
+            if (favicon) {
+                favicon.href = mode === "dark" ? "/see.me-light.ico" : "/see.me-dark.ico";
+            }
+        }, 0);
+
+        return () => clearTimeout(timeoutId);
     }, [mode]);
 }
