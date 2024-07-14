29bb15e0e05de057ce6b6e9f62ab2981617fde24
Vune <nguyenvu2319@gmail.com>
2024-07-14 13:01:03 +0700

    fix: handle change icon with theme

--- src/hooks/useDynamicFavicon.ts
+++ src/hooks/useDynamicFavicon.ts
@@ -1,10 +1,10 @@
 import { useColorScheme } from "@mui/joy/styles";
-import { useEffect } from "react";
+import { useLayoutEffect } from "react";
 
 export function useDynamicFavicon() {
     const { mode } = useColorScheme();
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         const favicon = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
         if (favicon) {
             favicon.href = mode === "dark" ? "/see.me-light.ico" : "/see.me-dark.ico";
